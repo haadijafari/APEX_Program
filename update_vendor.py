0ba51c9dd73636ed6dc3d2bea77d6e65be1f076a
Hadi Jafari <haadijafari2003@gmail.com>
2025-12-25 01:24:00 +0330

    [STYLE] (static) Correct 'update_vendor.py' Format
    [*] Fix python file formatting using black formatter
    [*] Fix Typo in comments

--- update_vendor.py
+++ update_vendor.py
@@ -3,21 +3,57 @@ import os
 
 # Define what to copy: (Source inside node_modules, Destination inside static)
 files = [
-    # Boostrap CSS and JS
-    ('node_modules/bootstrap/dist/css/bootstrap.min.css', 'backend/static/vendor/bootstrap/css/bootstrap.min.css'),
-    ('node_modules/bootstrap/dist/css/bootstrap.min.css.map', 'backend/static/vendor/bootstrap/css/bootstrap.min.css.map'),
-    ('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', 'backend/static/vendor/bootstrap/js/bootstrap.bundle.min.js'),
-    ('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map', 'backend/static/vendor/bootstrap/js/bootstrap.bundle.min.js.map'),
+    # Bootstrap CSS and JS
+    (
+        "node_modules/bootstrap/dist/css/bootstrap.min.css",
+        "backend/static/vendor/bootstrap/css/bootstrap.min.css",
+    ),
+    (
+        "node_modules/bootstrap/dist/css/bootstrap.min.css.map",
+        "backend/static/vendor/bootstrap/css/bootstrap.min.css.map",
+    ),
+    (
+        "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
+        "backend/static/vendor/bootstrap/js/bootstrap.bundle.min.js",
+    ),
+    (
+        "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map",
+        "backend/static/vendor/bootstrap/js/bootstrap.bundle.min.js.map",
+    ),
     # Chart.js
-    ('node_modules/chart.js/dist/chart.umd.min.js', 'backend/static/vendor/chartjs/js/chart.umd.min.js'),
-    ('node_modules/chart.js/dist/chart.umd.min.js.map', 'backend/static/vendor/chartjs/js/chart.umd.min.js.map'),
+    (
+        "node_modules/chart.js/dist/chart.umd.min.js",
+        "backend/static/vendor/chartjs/js/chart.umd.min.js",
+    ),
+    (
+        "node_modules/chart.js/dist/chart.umd.min.js.map",
+        "backend/static/vendor/chartjs/js/chart.umd.min.js.map",
+    ),
     # Emoji Picker Element
-    ('node_modules/emoji-picker-element/index.js', 'backend/static/vendor/emoji-picker/index.js'),
-    ('node_modules/emoji-picker-element/picker.js', 'backend/static/vendor/emoji-picker/picker.js'),
-    ('node_modules/emoji-picker-element/picker.js.map', 'backend/static/vendor/emoji-picker/picker.js.map'),
-    ('node_modules/emoji-picker-element/database.js', 'backend/static/vendor/emoji-picker/database.js'),
-    ('node_modules/emoji-picker-element/database.js.map', 'backend/static/vendor/emoji-picker/database.js.map'),
-    ('node_modules/emoji-picker-element-data/en/emojibase/data.json', 'backend/static/vendor/emoji-picker/data.json'),
+    (
+        "node_modules/emoji-picker-element/index.js",
+        "backend/static/vendor/emoji-picker/index.js",
+    ),
+    (
+        "node_modules/emoji-picker-element/picker.js",
+        "backend/static/vendor/emoji-picker/picker.js",
+    ),
+    (
+        "node_modules/emoji-picker-element/picker.js.map",
+        "backend/static/vendor/emoji-picker/picker.js.map",
+    ),
+    (
+        "node_modules/emoji-picker-element/database.js",
+        "backend/static/vendor/emoji-picker/database.js",
+    ),
+    (
+        "node_modules/emoji-picker-element/database.js.map",
+        "backend/static/vendor/emoji-picker/database.js.map",
+    ),
+    (
+        "node_modules/emoji-picker-element-data/en/emojibase/data.json",
+        "backend/static/vendor/emoji-picker/data.json",
+    ),
 ]
 
 for src, dest in files:
