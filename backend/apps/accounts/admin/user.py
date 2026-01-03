d38b4d88f7dc5abb4b006b677df69ec156cc8de7
Hadi Jafari <haadijafari2003@gmail.com>
2026-01-04 02:46:28 +0330

    [ADD] (accounts) Add New User Admin
    [+] Create new user admin based on new profiles and stats models

--- backend/apps/accounts/admin/user.py
+++ backend/apps/accounts/admin/user.py
@@ -2,22 +2,26 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from apps.accounts.models import User
-from apps.profiles.admin import PlayerProfileInline, PlayerStatsInline
+from apps.profiles.admin import PlayerProfileInline
 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    # Add both inlines
-    inlines = (PlayerProfileInline, PlayerStatsInline)
+    # Add the Profile inline
+    inlines = (PlayerProfileInline,)
 
-    list_display = ("username", "get_level", "get_rank", "is_staff")
+    list_display = ("username", "email", "get_level", "get_rank", "is_staff")
+
+    # Add these to list_select_related to avoid 100+ database queries
+    list_select_related = ("profile",)
 
     def get_level(self, obj):
-        return obj.profile.level
+        # Use getattr to avoid crash if a user has no profile
+        return getattr(obj.profile, "level", "-")
 
     get_level.short_description = "Level"
 
     def get_rank(self, obj):
-        return obj.profile.rank
+        return getattr(obj.profile, "rank", "-")
 
     get_rank.short_description = "Rank"
