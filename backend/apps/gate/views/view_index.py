b9c2ca2839f5b0a328d777574cf3134ea069130e
Hadi Jafari <haadijafari2003@gmail.com>
2026-01-26 18:34:32 +0330

    [REFACTOR (templates) Change Template Striucture
    [+] _ for includes
    [+] common for common files which are included in layouts or whole
    project
    [+] layouts for base, email, etc.

--- backend/apps/gate/views/view_index.py
+++ backend/apps/gate/views/view_index.py
@@ -23,7 +23,7 @@ class IndexView(LoginRequiredMixin, TemplateView):
     Aggregates Player Stats and Calendar Service data.
     """
 
-    template_name = "gate/index.html"
+    template_name = "index/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
