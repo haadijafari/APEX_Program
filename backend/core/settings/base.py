f33be48b8f18060e53d35a2b5bae07d16e921e5b
Hadi Jafari <haadijafari2003@gmail.com>
2026-02-03 21:48:10 +0330

    [FIX] (tasks) Tiny MCE Warning
    [+] Add Licence GPL to Tine MCE Settings

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -243,6 +243,7 @@ TINYMCE_DEFAULT_CONFIG = {
     "theme": "silver",
     "height": 300,
     "menubar": True,
+    "license_key": "gpl",
     "plugins": "advlist autolink lists link image charmap preview anchor "
     "searchreplace visualblocks code fullscreen insertdatetime media table help wordcount",
     "toolbar": "undo redo | formatselect | "
