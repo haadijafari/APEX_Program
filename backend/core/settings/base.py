945ed301d36f0bb729f59554622fc66aee5ccb37
Hadi Jafari <haadijafari2003@gmail.com>
2026-02-09 04:56:02 +0330

    [FIX] (docker) Add New Docker Configuration
    [+] Edit docker config
    [+] Create new .env structure

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -269,8 +269,7 @@ else:
     CACHES = {
         "default": {
             "BACKEND": "django_redis.cache.RedisCache",
-            # "LOCATION": "redis://redis:6379/1",  # uncomment for docker
-            "LOCATION": "redis://127.0.0.1:6379/1",
+            "LOCATION": os.environ.get("REDIS_URL", "redis://127.0.0.1:6379/1"),
             "OPTIONS": {
                 "CLIENT_CLASS": "django_redis.client.DefaultClient",
             },
--- backend/core/settings/dev.py
+++ backend/core/settings/dev.py
@@ -6,6 +6,13 @@ ALLOWED_HOSTS = [
     "*",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://0.0.0.0:8000",
+]
+# -
+
 INTERNAL_IPS = [
     "127.0.0.1",
     "localhost",
